86a6e9cbff738f7915e08fac3b9c2f8ffc487859
Flippergail <ticarter11@icloud.com>
2023-03-17 21:25:00 +0000

    Landing Page on Mobile

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,24 @@ export default function Home () {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col justify-center items-center min-h-screen max-h-screen">
-        <div className="absolute h-screen w-screen z-[-1]">
+      <main className="flex flex-col justify-end md:justify-center items-center min-h-screen max-h-screen bg-[#DF5644] md:bg-white">
+        <div className="absolute h-screen w-screen z-[1] hidden md:block ">
           <Image
               src="/Images/LandingBG.jpg"
               alt="Background Image"
               fill
-              className="-z-10 object-cover"
+              className="-z-10 object-contain"
           />
         </div>
-        <Link passHref className="md:w-50  lg:w-80 aspect-[36/12] p-2 font-semibold text-themeBlue hover:bg-[#FF9800] bg-themeOrange rounded-3xl text-center sm:text-3xl md:text-6xl lg:text-8xl" href="/play">
+        <div className="absolute h-screen w-screen z-[1] md:hidden">
+          <Image
+              src="/Images/MobileLandingBG.jpg"
+              alt="Background Image"
+              fill
+              className="-z-10 object-contain"
+          />
+        </div>
+        <Link passHref className="z-[2] w-60 mb-32 md:w-60 lg:w-80 p-2 md:p-3 lg:p-5 font-semibold text-[#860C0C] md:text-themeBlue hover:bg-[#FF9800] bg-themeOrange rounded-2xl md:rounded-3xl text-center text-7xl md:text-8xl lg:text-9xl" href="/play">
           <button>Play</button>
         </Link>
       </main>
