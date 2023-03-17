6da0462d39d2b2919f3b8d3f0941f4056ac33cdf
Flippergail <ticarter11@icloud.com>
2023-03-17 21:48:52 +0000

    Minor adjustments to mobile landing page

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home () {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col justify-end md:justify-center items-center min-h-screen max-h-screen bg-[#DF5644] md:bg-white">
+      <main className="flex flex-col justify-between md:justify-center items-center min-h-screen max-h-screen bg-[#DF5644] md:bg-white">
         <div className="absolute h-screen w-screen z-[1] hidden md:block ">
           <Image
               src="/Images/LandingBG.jpg"
@@ -28,7 +28,7 @@ export default function Home () {
               className="-z-10 object-contain"
           />
         </div>
-        <p className="md:hidden justify-self-start z-[2] pb-2 font-bold text-white text-center text-6xl">SnellRun</p>
+        <p className="md:hidden z-[2] mt-40 font-bold text-white text-7xl">SnellRun</p>
         <Link passHref className="z-[2] w-60 mb-32 md:mb-0 md:mt-36 lg:w-72 p-2 md:p-3 lg:p-5 font-semibold text-[#860C0C] md:text-themeBlue hover:bg-[#FF9800] bg-themeOrange rounded-2xl md:rounded-3xl text-center text-6xl md:text-7xl lg:text-8xl" href="/play">
           <button>Play</button>
         </Link>
