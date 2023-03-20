0c2f613b23694fbe6eed568fa1e51a108cb92ea2
Flippergail <ticarter11@icloud.com>
2023-03-20 20:02:12 +0000

    Update navbar.jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,24 +1,21 @@
 import Link from 'next/link'
-import Image from "next/image";
 import React from "react";
 
 export default function NavBar() {
     return(
         <div className="top-0 p-4 justify-between items-center flex h-[11%] md:h-[13%] relative w-full z-50">
             <div className="relative h-full aspect-square">
-                <Image
+                <img
                     src="/Images/OrangeHeaderIcon.png"
                     alt="Clifton Logo"
-                    fill
                 />
             </div>
 
             <p className="md:self-center text-themeBlue text-6xl md:text-8xl font-semibold">SnellRun</p>
             <Link className="relative h-full aspect-square" href="/">
-                <Image
+                <img
                     src="/Images/NavExitButton.png"
                     alt="Back Button"
-                    fill
                 />
             </Link>
         </div>
