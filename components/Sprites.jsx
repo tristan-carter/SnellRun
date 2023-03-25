8559c9ffa11038845d9e1e9cd687a2ecc551fdce
Flippergail <ticarter11@icloud.com>
2023-03-25 17:18:04 +0000

    Adding jump animations to both SnellSprite and TomSprite

--- components/Sprites.jsx
+++ components/Sprites.jsx
@@ -1,42 +1,47 @@
 import React, { useRef, useState } from 'react'
-import { Canvas, useLoader, useFrame } from '@react-three/fiber'
-import { useSpring, animated, config } from '@react-spring/three'
+import { useLoader, useFrame } from '@react-three/fiber'
+import { useSpring, animated } from '@react-spring/three'
 import * as THREE from "three";
 
+const startJumpingEvent = new Event('startJumping');
+
 function useSpritesheetAnimation (texture, frameTime) {
-    const t = useRef(0)
-    const currentFrame = useRef(0)
-  
-    useFrame((_, delta) => {
-      t.current += delta * 1000
-  
-      if (t.current >= frameTime) {
-        currentFrame.current += 1
-  
-        if (currentFrame.current >= 4) {
-          currentFrame.current = 0
-        }
-  
-        t.current = 0
-  
-        texture.offset.x = currentFrame.current / 4
+  const t = useRef(0)
+  const currentFrame = useRef(0)
+
+  useFrame((_, delta) => {
+    t.current += delta * 1000
+
+    if (t.current >= frameTime) {
+      currentFrame.current += 1
+
+      if (currentFrame.current >= 4) {
+        currentFrame.current = 0
       }
-    })
-  
-    return { t, currentFrame }
-  }
+
+      t.current = 0
+
+      texture.offset.x = currentFrame.current / 4
+    }
+  })
+
+  return { t, currentFrame }
+}
+
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
 
 export function TomSprite() {
-  const jumping = useRef(false)
+  const spriteRef = useRef(null);
+  const [jumping, setJumping] = useState(false);
 
-  const [springs, api] = useSpring(() => ({
-    position: [-1.5, -0.5, 1],
-    config: {
-      tension: 170,
-      friction: 26,
-      mass: 1,
-    },
-  }));
+  const springs = useSpring({
+    position: jumping ? [-0.8, 0.7, 1] : [-0.8, -0.6, 1],
+    config: { tension: 120, friction: 14, duration: 500 }
+  })
 
   const texture = useLoader(THREE.TextureLoader, '/Images/TomSpriteSet.png')
   texture.minFilter = THREE.NearestFilter
@@ -46,14 +51,14 @@ export function TomSprite() {
 
   useSpritesheetAnimation(texture, 180)
 
-  function jumpPressed(){
-    if (!jumping.current){
-      jumping.current = true
-      console.log(jumping.current)
-      api.start({ position: [-1.5, 1.5, 1] });
-      //set({ position: [-1.5, -0.5, 1] });
-      //jumping.current = false
-      //console.log(jumping.current)
+  async function jumpPressed(){
+    if (spriteRef.current) {
+      if (spriteRef.current.position.y == -0.6){
+        window.dispatchEvent(startJumpingEvent)
+        setJumping(true)
+        await delay(500)
+        setJumping(false)
+      }
     }
   }
 
@@ -62,24 +67,47 @@ export function TomSprite() {
   
   
   return (
-      <animated.sprite scale={[1.9, 1.9]} position={[-0.8, -0.6, 1]} >
+      <animated.sprite scale={[1.9, 1.9]} position={springs.position} ref={spriteRef} >
           <spriteMaterial transparent map={texture} />
       </animated.sprite>
   )
 }
 
 export function SnellSprite() {
+  const spriteRef = useRef();
+  const [jumping, setJumping] = useState(false);
+
+  const springs = useSpring({
+    position: jumping ? [-1.7, 0.7, 1] : [-1.7, -0.5, 1],
+    config: { tension: 120, friction: 14, duration: 500 }
+  })
+
   const texture = useLoader(THREE.TextureLoader, '/Images/SnellSpriteSet.png')
   texture.minFilter = THREE.NearestFilter
   texture.magFilter = THREE.NearestFilter
 
   texture.repeat.set(1/4, 1)
 
+  async function jumpPressed(){
+    if (spriteRef) {
+      if (spriteRef.current.position.y == -0.5){
+        await delay(100)
+        setJumping(true)
+        await delay(500)
+        setJumping(false)
+      }
+    }
+  }
+
+  window.addEventListener("startJumping", jumpPressed);
+
+
+
   useSpritesheetAnimation(texture, 200)
   
   return (
-      <sprite scale={[2.1, 2.1]} position={[-1.5, -0.5, 1]} >
+      <animated.sprite scale={[2.1, 2.1]} position={springs.position} ref={spriteRef} >
           <spriteMaterial transparent map={texture} />
-      </sprite>
+      </animated.sprite>
   )
 }
