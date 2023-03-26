aea3d5dd287260d0e36d55071628be179d77cb52
Flippergail <ticarter11@icloud.com>
2023-03-26 21:53:59 +0100

    adding collisions

---
+++ pages/gameover.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import React from 'react'
+import { Canvas, useLoader, KeyboardControls } from '@react-three/fiber'
+import * as THREE from "three";
+
+import UIfx from 'uifx';
+import mp3File from './my-sounds/beep.mp3';
+
+import NavBar from '/components/navbar'
+
+export default function Retry () {
+    return (
+        <>
+            <Head>
+                <title>SnellRun</title>
+                <meta name="description" content="2d game escaping Mr Snelling because you are late. Credits: Tristan C, Alexa G, Tom G, Adam L and to everyone who gave suggestions." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/Images/HeaderIcon.png" />
+            </Head>
+            
+            <main className="flex flex-col items-center h-full bg-themeBlue">
+                <NavBar/>
+
+                <div className="absolute h-full w-screen z-[1] rounded-lg "> 
+                    <Image
+                        src="/Images/SnellingCross.png"
+                        alt="Background Image"
+                        fill
+                        className="-z-10 object-contain"
+                        priority={true}
+                    />
+                </div>
+
+                <p className="z-[2] self-center mt-[70%] md:mt-[14%] font-bold text-[#E50000] text-9xl">Late!!</p>
+                <Link passHref className="z-[2] w-70 md:mb-0 md:mt-36 lg:w-70 p-2 md:p-3 lg:p-4 font-semibold text-white hover:bg-[#55748F] bg-themeBlue rounded-2xl md:rounded-3xl text-center text-6xl md:text-7xl lg:text-8xl" href="/play">
+                    <button>Retry</button>
+                </Link>
+            </main>
+        </>
+    );
+}
--- pages/play.js
+++ pages/play.js
@@ -9,6 +9,7 @@ import NavBar from '/components/navbar'
 import { TomSprite } from '/components/Sprites';
 import { SnellSprite } from '/components/Sprites';
 import { Obstacle } from '/components/ObstacleManager';
+import { useRouter } from 'next/navigation';
 
 function Background({ image }) {
   const texture = useLoader(THREE.TextureLoader, image);
@@ -22,27 +23,31 @@ function Background({ image }) {
   );
 }
 
-function checkCollision(obj1, obj2) {
+function checkCollision(sprite, obstacle) {
   return (
-    obj1.position.x < obj2.position.x + obj2.width &&
-    obj1.position.x + obj1.width > obj2.position.x &&
-    obj1.position.y < obj2.position.y + obj2.height &&
-    obj1.height + obj1.position.y > obj2.position.y
+    sprite.position.x < obstacle.position.x + 0.7 &&
+    sprite.position.x + 0.8 > obstacle.position.x &&
+    sprite.position.y < obstacle.position.y + 1 &&
+    1.9 + sprite.position.y > obstacle.position.y
   );
 }
 
 function Game () {
+  const router = useRouter();
+
   const spriteRef = useRef(null)
 
   const obstacleRef1 = useRef(null)
   const obstacleRef2 = useRef(null)
   const obstacleRef3 = useRef(null)
+  
 
   useFrame(() => {
     if (checkCollision(spriteRef.current, obstacleRef1.current) 
     || checkCollision(spriteRef.current, obstacleRef2.current) 
     || checkCollision(spriteRef.current, obstacleRef3.current)) {
-      console.log('collision detected!');
+      // collided with an obstacle so takes you to the game over page
+      router.push('/gameover')
     }
   });
   return (
@@ -71,9 +76,9 @@ export default function Play () {
       </Head>
       <main className="flex flex-col justify-center items-center h-full bg-themeBlue">
         <NavBar/>
-          <Canvas className='bg-[#41BFA7]'>
-            <Game/>
-          </Canvas>
+        <Canvas className='bg-[#41BFA7]'>
+          <Game/>
+        </Canvas>
       </main>
     </>
   )
