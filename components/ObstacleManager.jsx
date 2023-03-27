fa7f2db631c9ab772218c73b89ef6790b87147b6
Flippergail <ticarter11@icloud.com>
2023-03-27 22:04:37 +0100

    increasing difficulty

--- components/ObstacleManager.jsx
+++ components/ObstacleManager.jsx
@@ -24,11 +24,13 @@ export function ObstacleManager({ref1, ref2, ref3}) {
     const speedRef = useRef(4.5)
 
     const lastSentTimeRef = useRef(Date.now())
+    const startTime = Date.now()
     const waitTimeRef = useRef(5000)
 
     const ref1Anchored = useRef(true)
     const ref2Anchored = useRef(true)
     const ref3Anchored = useRef(true)
+
 
     useFrame((_, delta) => {
         if (!ref1Anchored.current) {
@@ -55,22 +57,36 @@ export function ObstacleManager({ref1, ref2, ref3}) {
 
         const currentTime = Date.now()
         if (lastSentTimeRef.current+waitTimeRef.current < currentTime) {
+            const minDelay = 600
+            const possibleExtraDelay = 800
+
+            if (currentTime-startTime>30000) {
+                minDelay = 400
+                possibleExtraDelay = 500
+            } else if (currentTime-startTime>20000) {
+                minDelay = 500
+                possibleExtraDelay = 500
+            } else if (currentTime-startTime>10000) {
+                minDelay = 500
+                possibleExtraDelay = 600
+            }
+
             if (ref1Anchored.current) {
                 ref1.current.position.x = 4
                 lastSentTimeRef.current = currentTime
-                waitTimeRef.current = Math.floor(Math.random() * 800)+600;
+                waitTimeRef.current = Math.floor(Math.random() * possibleExtraDelay)+minDelay;
                 
                 ref1Anchored.current = false
             } else if (ref2Anchored.current) {
                 ref2.current.position.x = 4
                 lastSentTimeRef.current = currentTime
-                waitTimeRef.current = Math.floor(Math.random() * 800)+600;
+                waitTimeRef.current = Math.floor(Math.random() * possibleExtraDelay)+minDelay;
 
                 ref2Anchored.current = false
             } else if (ref3Anchored.current) {
                 ref3.current.position.x = 4
                 lastSentTimeRef.current = currentTime
-                waitTimeRef.current = Math.floor(Math.random() * 800)+600;
+                waitTimeRef.current = Math.floor(Math.random() * possibleExtraDelay)+minDelay;
 
                 ref3Anchored.current = false
             }
