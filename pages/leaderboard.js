f3addd9c78c65e2c5517b5d85a8768b09523f161
Flippergail <ticarter11@icloud.com>
2023-03-29 08:16:21 +0100

    Update leaderboard.js

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -9,13 +9,13 @@ import * as THREE from "three";
 import NavBar from '/components/navbar'
 
 const LeaderboardStats = [
-    { position: "4", name: "Josh P", score: "1363" },
-    { position: "5", name: "Olly S", score: "1056" },
-    { position: "6", name: "Tristan C", score: "743" },
-    { position: "7", name: "Mitch M", score: "503" },
-    { position: "8", name: "?", score: "..." },
-    { position: "9", name: "?", score: "..." },
-    { position: "10", name: "?", score: "..." },
+    { position: "4", name: "Tom G", score: "1943" },
+    { position: "5", name: "Josh P", score: "1363" },
+    { position: "6", name: "Olly S", score: "1056" },
+    { position: "7", name: "Tristan C", score: "743" },
+    { position: "8", name: "Mitch M", score: "503" },
+    { position: "9", name: "Elizabeth W", score: "401" },
+    { position: "10", name: "Gabriel D", score: "348" },
     { position: "11", name: "?", score: "..." },
     { position: "12", name: "?", score: "..." },
     { position: "13", name: "?", score: "..." },
@@ -38,13 +38,13 @@ export default function Leaderboard () {
                 <p className="mt-4 font-bold text-[#D6973A] text-4xl md:text-7xl">🏆Leaderboard🏆</p>
                 <ul className="mt-4" >
                     <li className="m-0.5 text-[#b8860b] text-3xl md:text-4xl font-bold text-center">
-                        1st - Roman Z :  3056 points
+                        1st - Adam L :  5671 points
                     </li>
                     <li className="m-0.5 text-[#c0c0c0] text-3xl md:text-4xl font-bold text-center">
-                        2nd - Conor H :  2252 points
+                        2nd - Roman Z :  3056 points
                     </li>
                     <li className="m-0.5 text-[#cd7f32] text-3xl md:text-4xl font-bold text-center">
-                        3rd - Adam L :  1611 points
+                        3rd - Conor H :  2252 points
                     </li>
                     {LeaderboardStats.map(User => <li className="m-0.5 text-white text-2xl md:text-3xl font-semibold text-center">
                         {User["position"]}th - {User["name"]} : {User["score"]} points
