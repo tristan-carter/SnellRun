b49e9c971b29efed81cfa4b76fdcc10dbe98e618
Flippergail <ticarter11@icloud.com>
2023-04-18 18:23:52 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import '@/styles/globals.css'
 import React from 'react'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function App ({ Component, pageProps }) {
   return( 
-  <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>
+  <Component {...pageProps} />
   )
 }
